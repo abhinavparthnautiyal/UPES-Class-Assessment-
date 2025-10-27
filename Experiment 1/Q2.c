57749cdf6f20eecf5cb02d8706958378e26a4845
abhinavparthnautiyal <abhinav07nautiyal@gmail.com>
2025-10-27 22:41:35 +0530

    Installation, Environment Setup and starting with C language

---
+++ Experiment 1/Q2.c
@@ -0,0 +1,12 @@
+/*2.	Write a C Program to print the address in multiple lines (new line).*/
+#include <stdio.h>
+
+int main() 
+{
+    printf("Abhinav Parth Nautiyal\n");
+    printf("123, Green Valley Road\n");
+    printf("Dehradun, Uttarakhand\n");
+    printf("India - 248001\n");
+    
+    return 0;
+}
