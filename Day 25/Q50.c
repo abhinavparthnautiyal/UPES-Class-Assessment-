c7aedd8ac02df4e68279e064278c38dc1fc1dc8b
abhinavparthnautiyal <abhinav07nautiyal@gmail.com>
2025-10-28 18:42:51 +0530

    (Nested Loops without Arrays/Strings)

---
+++ Day 25/Q50.c
@@ -0,0 +1,40 @@
+/*
+Q50: Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+    
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+ ****
+  ***
+   **
+    *
+
+Input 2:
+
+Output 2:
+Note: Spaces indicate indentation.
+*/
+#include <stdio.h>
+void main()
+{
+    int i, j, space;
+    for(i = 1; i <= 5; i++) 
+    {
+        for(space = 1; space < i; space++) 
+        {
+            printf(" ");
+        }
+        for(j = 5; j >= i; j--) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
